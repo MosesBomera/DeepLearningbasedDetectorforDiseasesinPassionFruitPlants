3ec35e2b0eb861b1e774b90b63862d6c77999f25
Moses Bomera <mosesbomera@gmail.com>
2020-09-22 11:26:13 +0300

    Update the screen resolution

--- classification.py
+++ classification.py
@@ -71,7 +71,7 @@ def main():
   interpreter.allocate_tensors()
   _, height, width, _ = interpreter.get_input_details()[0]['shape']
 
-  with picamera.PiCamera(resolution=(640, 480), framerate=30) as camera:
+  with picamera.PiCamera(resolution=(320, 240), framerate=30) as camera:
     camera.start_preview()
     try:
       stream = io.BytesIO()
