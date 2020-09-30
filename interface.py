9cd7177807990fdbc90e6840d0730d52e59292da
Moses Bomera <mosesbomera@gmail.com>
2020-09-30 21:23:25 +0300

    Test simple image capture

--- interface.py
+++ interface.py
@@ -36,39 +36,52 @@ from tflite_runtime.interpreter import Interpreter
 
 # Main function
 def main():
-    take_picture()
-
-# Utility functions
-def load_labels(path):
-  with open(path, 'r') as f:
-    return {i: line.strip() for i, line in enumerate(f.readlines())}
-
-def set_input_tensor(interpreter, image):
-  tensor_index = interpreter.get_input_details()[0]['index']
-  input_tensor = interpreter.tensor(tensor_index)()[0]
-  input_tensor[:, :] = image
-
-def classify_image(interpreter, image, top_k=1):
-  """Returns a sorted array of classification results."""
-  set_input_tensor(interpreter, image)
-  interpreter.invoke()
-  output_details = interpreter.get_output_details()[0]
-  output = np.squeeze(interpreter.get_tensor(output_details['index']))
-
-  # If the model is quantized (uint8 data), then dequantize the results
-  if output_details['dtype'] == np.uint8:
-    scale, zero_point = output_details['quantization']
-    output = scale * (output - zero_point)
-
-  ordered = np.argpartition(-output, top_k)
-  return [(i, output[i]) for i in ordered[:top_k]]
-
-def take_picture():
     camera = picamera.PiCamera()
     camera.start_preview()
-    sleep(5)
+    sleep(10)
     camera.capture('/home/pi/Desktop/image.jpg')
     camera.stop_preview()
+
+
+class Utils:
+    # Utility functions
+    def load_labels(path):
+      with open(path, 'r') as f:
+        return {i: line.strip() for i, line in enumerate(f.readlines())}
+
+    def take_picture():
+        camera = picamera.PiCamera()
+        camera.start_preview()
+        sleep(5)
+        camera.capture('/home/pi/Desktop/image.jpg')
+        camera.stop_preview()
+
+class Classifier:
+    def __init__(self, model, labels):
+        self.labels = labels
+        self.model = Interpreter(model)
+
+    def set_input_tensor(self, image):
+      tensor_index = self.model.get_input_details()[0]['index']
+      input_tensor = self.model.tensor(tensor_index)()[0]
+      input_tensor[:, :] = image
+
+    def classify_image(self, image, top_k=1):
+      """Returns a sorted array of classification results."""
+      set_input_tensor(self.model, image)
+      self.model.invoke()
+      output_details = self.model.get_output_details()[0]
+      output = np.squeeze(self.model.get_tensor(output_details['index']))
+
+      # If the model is quantized (uint8 data), then dequantize the results
+      if output_details['dtype'] == np.uint8:
+        scale, zero_point = output_details['quantization']
+        output = scale * (output - zero_point)
+
+      ordered = np.argpartition(-output, top_k)
+      return [(i, output[i]) for i in ordered[:top_k]]
+
+class Button:
 
 # Programme
 if __name__ == '__main__':
